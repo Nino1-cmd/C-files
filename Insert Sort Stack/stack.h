14d8f296ba740ad09ee923f90b0d77f3557e510b
Nino1-cmd <garlicbreaddine@gmail.com>
2024-12-08 12:10:06 +0800

    Create stack.h

---
+++ Insert Sort Stack/stack.h
@@ -0,0 +1,43 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <stdbool.h>
+typedef struct {
+    char name[30];
+    int age;
+    char sex;
+} Person;
+
+typedef struct node{
+    Person data;
+    struct node *link;
+} NodeType, *NodePtr, *Stack;
+
+Person createPerson(char name[30], int age, char sex);
+void initStack(Stack *s);
+bool isEmpty(Stack s);
+void displayStack1(Stack s);
+void displayStack2(Stack s); //no traversal, push, pop, peek
+
+NodePtr createNode(Person p);
+bool push(Stack *s, Person p);
+bool pop(Stack *s);
+Person peek(Stack s);
+
+/*Insert sorted base on lastname. Rember to use the property 
+  of the stack using push, pop, and peek functions.*/
+bool insertSorted1(Stack *s, Person p);
+
+/*Insert sorted base on lastname. Rember to use the property 
+  of the stack but without using push, pop, and peek functions.*/
+bool insertSorted2(Stack *s, Person p);
+
+/*Sort the values based on name. Use flag for ascending 
+  or descending. */
+void sortStack1(Stack *s, bool flag);
+
+/*Sort the values based on name. Use flag for ascending 
+  or descending. NO PUSH POP PEEK AND NO TRAVERSAL*/
+void sortStack2(Stack *s, bool flag);
+
+#endif
