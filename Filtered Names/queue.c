754851948ce598c3beda478bd218acd27998c837
Nino1-cmd <garlicbreaddine@gmail.com>
2024-12-08 12:06:06 +0800

    Create queue.c

---
+++ Filtered Names/queue.c
@@ -0,0 +1,175 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include "queue.h"
+
+#define MAX 8
+
+Name createName(char fName[], char lName[]){
+    Name n;
+    strcpy(n.fName, fName);
+    strcpy(n.lName, lName);
+    return n;
+}
+
+void initNQueue(NQueue *nq){
+    nq->head = 1;
+    nq->tail = 0;
+}
+
+bool isEmpty(NQueue nq){
+    return (nq.head == (nq.tail + 1) % MAX) ? true : false; 
+}
+
+bool isFull(NQueue nq){
+    return (nq.head == (nq.tail + 2) % MAX) ? true : false; 
+}
+
+void displayQueue1(NQueue nq){
+    printf("DATA: {\n");
+    while(!isEmpty(nq)){
+        Name n = front(nq);
+        printf("%s, %s\n", n.lName, n.fName);
+        dequeue(&nq);
+    }
+    printf("}\n");
+}
+
+void displayQueue2(NQueue nq){
+    printf("DATA: {\n");
+    
+    NQueue temp;
+    initNQueue(&temp);
+    
+    int i = nq.head;
+    int tempTail = temp.head;
+    
+    for(; i != (nq.tail + 1) % MAX; i = (i + 1) % MAX){
+        printf("%s, %s\n", nq.elems[i].lName, nq.elems[i].fName);
+        temp.elems[tempTail] = nq.elems[i];
+        tempTail = (tempTail + 1) % MAX;
+    }
+
+    temp.tail = (tempTail - 1 + MAX) % MAX;
+
+    i = temp.head;
+    tempTail = nq.head;
+    for(; i != (temp.tail + 1) % MAX; i = (i + 1) % MAX){
+        nq.elems[tempTail] = temp.elems[i];
+        tempTail = (tempTail + 1) % MAX;
+    }
+    
+    printf("}\n");
+}
+
+bool enqueue(NQueue *nq, Name n){
+    if(!isFull(*nq)){
+        nq->tail = (nq->tail + 1) % MAX;
+        nq->elems[nq->tail] = n;
+        return true;
+    }
+    return false;
+}
+
+bool dequeue(NQueue *nq){
+    if(!isEmpty(*nq)){
+        nq->head = (nq->head + 1) % MAX;
+        return true;
+    }
+    return false; 
+}
+
+Name front(NQueue nq){
+    return nq.elems[nq.head]; 
+}
+
+int countElem(NQueue nq){
+    int count = 0;
+    while(!isEmpty(nq)){
+      count++;
+      dequeue(&nq);
+    }
+    return count;
+}
+
+/*Removes the names that matches the filterString to the lastname. 
+  Returns the removed names. Use the concept of adding a sentinel
+  at the end indicating the last item in list as empty strings for
+  fname and lname. If there are no names that will match then the
+  function should return the sentinel.*/
+Name *filterNames(NQueue *nq, char *filterString){
+    int count = 0;
+    int size = countElem(*nq);
+    NQueue temp;
+    initNQueue(temp); 
+    Name *nameList = (Name*)malloc(sizeof(Name)*size+1);
+    
+    while(nq.head == (nq.tail + 1) % MAX) {
+        Name Filter = front(*nq); // getting the front value of the stack
+            if(strcmp(filter.lName, filterString) == 0){ //check for similarities
+                nameList[count++] = filter; //collect the value
+                dequeue(nq);
+            }else {
+                enqueue(&temp, filter);
+                dequeue(nq);
+            }
+    }
+    
+    strcpy(nameList[count].fName,""); //adds a sentinel value
+    strcpy(nameList[count++].lName,""); //adds a sentinel value
+    
+    nameList = (Name*)realloc(nameList,count*sizeof(Name));//updates size for sentinel values
+    
+    while(nq.head == (nq.tail + 1) % MAX) {
+        Name n = front(temp);
+        enqueue(nq, n);
+        dequeue(&temp);
+    }
+    return nameList;
+}
+
+void displayNames(Name nameArray[]){
+    printf("NAME DATA: {\n");
+    for(int i = 0; strcmp(nameArray[i].fName, "") != 0; ++i){
+        printf("%s, %s\n", nameArray[i].lName, nameArray[i].fName);
+    }
+    printf("}\n");
+}
+
+/*Insert sorted base on lastname. Remember to use the property 
+  of the queue but without using the functions (enqueue, dequeue, front)*/
+bool insertSorted(NQueue *nq, Name n){
+    if(isFull(*nq)){
+        return false;
+    }
+    
+    NQueue temp;
+    initNQueue(&temp);
+    
+    int i = nq->head;
+    int tempTail = temp.head;
+    
+    for(; i != (nq->tail + 1) % MAX && strcmp(nq->elems[i].lName, n.lName) < 0; i = (i + 1) % MAX){
+        temp.elems[tempTail] = nq->elems[i];
+        tempTail = (tempTail + 1) % MAX;
+    }
+    
+    temp.elems[tempTail] = n;
+    tempTail = (tempTail + 1) % MAX;
+    
+    while (i != (nq->tail + 1) % MAX) {
+        temp.elems[tempTail] = nq->elems[i];
+        tempTail = (tempTail + 1) % MAX;
+        i = (i + 1) % MAX;
+    }
+
+    temp.tail = (tempTail - 1 + MAX) % MAX;
+
+    nq->head = temp.head;
+    nq->tail = temp.tail;
+    for (int j = temp.head; j != (temp.tail + 1) % MAX; j = (j + 1) % MAX) {
+        nq->elems[j] = temp.elems[j];
+    }
+    return true;
+}
